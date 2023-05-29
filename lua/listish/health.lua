97bc1ec9fc09a572124e19415c2bdd865a2f592f
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-29 19:43:52 +0100

    fix(health): deprecated health functions

--- lua/listish/health.lua
+++ lua/listish/health.lua
@@ -1,19 +1,18 @@
 local M = {}
-local health = vim.health
 
 local libs = {
   arshlib = "arsham/arshlib.nvim",
 }
 
 M.check = function()
-  health.report_start("Listish Health Check")
+  vim.health.start("Listish Health Check")
   for name, package in pairs(libs) do
     if not pcall(require, name) then
-      health.report_error(package .. " was not found", {
+      vim.health.error(package .. " was not found", {
         'Please install "' .. package .. '"',
       })
     else
-      health.report_ok(package .. " is installed")
+      vim.health.ok(package .. " is installed")
     end
   end
 end
